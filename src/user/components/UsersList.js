8c4112ef2b26c60a9b7dde8ecbb095b5ef80867c
haha <haha@pm.me>
2022-05-06 10:15:24 +0545

    user requests

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -22,7 +22,7 @@ const UsersList = props => {
           key={user.id}
           id={user.id}
           image={user.image}
-          name={user.name}
+          name={user.username}
           placeCount={user.places}
         />
       ))}
